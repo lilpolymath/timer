1ce40ec07d3e04f70455bab124b531516084bd4c
SuiGeneris01 <ayofavour7@gmail.com>
2019-08-05 12:34:38 +0100

    Experimented with the API.

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,17 @@
 class TimersDashboard extends React.Component {
   state = {
-    timers: [
-      {
-        title: "Practice squat",
-        project: "Gym Chores",
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title: "Bake squash",
-        project: "Kitchen Chores",
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
+    timers: [],
+  };
+
+  componentDidMount() {
+    this.loadTimersFromServers();
+    setInterval(this.loadTimersFromServers, 5000);
+  }
+
+  loadTimersFromServers = () => {
+    client.getTimers(serverTimers => {
+      this.setState({ timers: serverTimers });
+    });
   };
 
   handleStartClick = timerId => {
